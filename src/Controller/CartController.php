3958146ad90deb30b915869a67016a3c7eb382b8
JonasDevEdgco <jonas@edgco.fr>
2022-09-18 15:21:34 +0200

    [Fix] : Création de la vue récapitulatif panier avant l'entrée en tunnel d'achat

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,19 +3,37 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+    
     /**
      * @Route("/panier", name="app_cart")
      */
     public function index(Cart $cart): Response
     {
-        dd($cart->get());
+        $cartComplete = [];
+
+        foreach ($cart->get() as $id => $quantity) {
+            $cartComplete[] = [
+                'product' => $this->em->getRepository(Product::class)->findOneById($id),
+                'quantity' => $quantity
+            ];
+        }
+
         return $this->render('cart/index.html.twig', [
+            'cart' => $cartComplete
         ]);
     }
 
