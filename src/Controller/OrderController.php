2f50e7cd737eab7d318bc667d946c190aaf003d4
JonasDevEdgco <jonas@edgco.fr>
2022-10-01 18:14:45 +0200

    [ADD] : Tunnel d'achat : Stocker les informations de la commande en base

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Order;
 use App\Form\OrderType;
+use App\Entity\OrderDetails;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +14,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class OrderController extends AbstractController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+    
     /**
      * @Route("/commande", name="app_order")
      */
-    public function index(Request $request, Cart $cart): Response
+    public function index(Cart $cart): Response
     {
         $user = $this->getUser();
 
@@ -28,22 +38,74 @@ class OrderController extends AbstractController
             'user' => $user // A voir l'user en cours
         ]);
 
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid()){
-
-            dd($form->getData());
-
-            // $address->setUser($this->getUser());
-
-            // $this->em->persist($address);
-            // $this->em->flush();
-            // return $this->redirectToRoute('app_account_address');
-        }
-
         return $this->render('order/index.html.twig', [
             'form' => $form->createView(),
             'cart' => $cart->getFull()
         ]);
+    }
+
+    /**
+     * @Route("/commande/recapitulatif", name="app_order_reacp", methods={"POST"})
+     */
+    public function add(Request $request, Cart $cart): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $user // A voir l'user en cours
+        ]);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $date = new \DateTime();
+
+            // $carriers = $form['carriers']->getData();
+            // $carriers = $form->getData()['carriers'];
+            $carriers = $form->get('carriers')->getData();
+            $delivery = $form->get('addresses')->getData();
+            $delivery_content = $delivery->getFirstname() . ' ' . $delivery->getLastname();
+            $delivery_content .= '<br/>' . $delivery->getPhone();
+
+            if ($delivery->getCompany()) {
+                $delivery_content .= '<br/>' . $delivery->getCompany();
+            }
+
+            $delivery_content .= '<br/>' . $delivery->getAddress();
+            $delivery_content .= '<br/>' . $delivery->getPostal() . ' ' . $delivery->getCity();
+            $delivery_content .= '<br/>' . $delivery->getCountry();
+            
+
+            $order = new Order();
+            $order->setUser($user);
+            $order->setCreatedAt($date);
+            $order->setCarrierName($carriers->getName());
+            $order->setCarrierPrice($carriers->getPrice());
+            $order->setDelivery($delivery_content);
+            $order->SetIsPaid(0);
+
+            foreach ($cart->getFull() as $key => $product) {
+                $orderDetails = new OrderDetails();
+                $orderDetails->setMyOrder($order);
+                $orderDetails->setProduct($product['product']->getName());
+                $orderDetails->setQuantity($product['quantity']);
+                $orderDetails->setPrice($product['product']->getPrice());
+                $orderDetails->setTotal($product['product']->getPrice() * $product['quantity']);
+
+                $this->em->persist($orderDetails);
+            }
+
+            $this->em->persist($order);
+            // $this->em->flush();
+
+            return $this->render('order/add.html.twig', [
+                'cart' => $cart->getFull(),
+                'carrier' => $carriers,
+                'delivery' => $delivery_content
+            ]);
+        }
+
+        return $this->redirectToRoute('app_cart');
     }
 }
