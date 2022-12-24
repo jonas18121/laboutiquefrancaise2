b398067209113a2e0899b5f286bf35694db514ea
JonasDevEdgco <jonas@edgco.fr>
2022-12-24 16:58:50 +0100

    [ADD] mailjet config

---
+++ src/Classe/Mail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Classe;
+
+class Mail
+{
+
+    public function send()
+    {
+        $mj = new \Mailjet\Client(getenv('MJ_APIKEY_PUBLIC'), getenv('MJ_APIKEY_PRIVATE'),true,['version' => 'v3.1']);
+        $body = [
+            'Messages' => [
+                [
+                    'From' => [
+                        'Email' => "pilot@mailjet.com",
+                        'Name' => "Mailjet Pilot"
+                    ],
+                    'To' => [
+                        [
+                            'Email' => "passenger1@mailjet.com",
+                            'Name' => "passenger 1"
+                        ]
+                    ],
+                    'Subject' => "Your email flight plan!",
+                    'TextPart' => "Dear passenger 1, welcome to Mailjet! May the delivery force be with you!",
+                    'HTMLPart' => "<h3>Dear passenger 1, welcome to <a href=\"https://www.mailjet.com/\">Mailjet</a>!</h3><br />May the delivery force be with you!"
+                ]
+            ]
+        ];
+        $response = $mj->post(Resources::$Email, ['body' => $body]);
+        $response->success() && var_dump($response->getData());  
+    }
+}
